995152c4f96e20406134c98c1e59dc44ad97c900
ebinpsunny890 <ebinpsunny890@gmail.com>
2024-04-22 15:22:27 +0530

    index.html file added

--- python-docker-master/static_app/server.py
+++ python-docker-master/static_app/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():  
-    data = "Hello, World"
+    data = "Helloo moto"
     return render_template('index.html', data=data)
 
 # run the application
